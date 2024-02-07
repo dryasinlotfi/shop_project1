a15e0e4f53b6ee1636bd131e5162100cfa98a494
Yasin psycho <yasinlotfi33@gmail.com>
2024-02-08 02:32:31 +0330

    create serializer.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
 # Create your views here.
+
+
+def all_todos(request: Request):
+    todos = Todo.objects.order_by('priority').all()
+    return Response()
+
+
+
